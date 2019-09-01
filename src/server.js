c47be4850fbba5f0614d53dd806f42e869298a4b
Alexander Savchenko <sasha.savchenko@gmail.com>
2019-09-02 01:02:13 +0300

    mongodb driver works

--- src/server.js
+++ src/server.js
@@ -4,8 +4,48 @@ import express from 'express';
 import { renderToString } from 'react-dom/server';
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
+const bodyParser = require("body-parser");
 
 const server = express();
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+(async function() {
+  // Connection URL
+  const url = process.env.MONGODB_URI;
+  // Database Name
+  const dbName = 'testdb';
+  const client = new MongoClient(url, { useNewUrlParser: true });
+
+  try {
+    // Use connect method to connect to the Server
+    await client.connect();
+    console.log("Connected correctly to server");
+
+    const db = client.db(dbName);
+    const col = db.collection('inserts');
+    
+    const r = await col.insertMany([{a:1}, {a:1}, {a:1}]);
+    assert.equal(3, r.insertedCount);
+
+    // Get the cursor
+    const cursor = col.find({a:1}).limit(2);
+
+    // Iterate over the cursor
+    while(await cursor.hasNext()) {
+      const doc = await cursor.next();
+      console.dir(doc);
+    }
+
+  } catch (err) {
+    console.log(err.stack);
+  }
+
+
+
+  client.close();
+})();
 
 server
   .disable('x-powered-by')
