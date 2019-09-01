283108ef31e9e662453d6550675e80a959ff9345
Alexander Savchenko <sasha.savchenko@gmail.com>
2019-09-01 22:52:21 +0300

    check another razzle config

--- razzle.config.js
+++ razzle.config.js
@@ -3,10 +3,9 @@
 const razzleHeroku = require('razzle-heroku');
 
 module.exports = {
-  plugins: ['scss'],
   modify(config, { target, dev }, webpack) {
     config = razzleHeroku(config, {target, dev}, webpack);
 
     return config;
-  }
+  },
 };
